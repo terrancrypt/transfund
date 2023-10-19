e8352398e88f4983d70f3f5de698d52ebc3128e8
terrancrypt <thaiphamngoctuong@gmail.com>
2023-10-19 23:10:30 +0700

    update

--- smart-contract/src/Vault/FundVault.sol
+++ smart-contract/src/Vault/FundVault.sol
@@ -6,23 +6,46 @@ import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import {ERC4626} from "@openzeppelin/contracts/token/ERC20/extensions/ERC4626.sol";
 import {IERC4626} from "@openzeppelin/contracts/interfaces/IERC4626.sol";
+import {Math} from "@openzeppelin/contracts/utils/math/Math.sol";
 
 contract FundVault is ERC4626Fees {
     error FundVault__MustBeOwner();
+    error FundVault__VaultIsFull();
+    error FundVault__OwnerCantWithdraw();
 
     address payable public immutable i_vaultOwner;
     uint256 public s_entryFeeBasicPoints;
     uint256 public s_ownerShares;
-    uint256 private immutable i_ownerSharesPercentage;
+    uint256 public immutable i_ownerSharesPercentage;
+    uint256 public s_totalSharesCanMint;
 
     constructor(
         IERC20 _asset,
-        uint256 _basicPoints,
+        uint256 _feeBasicPoints,
         uint256 _ownerSharesPercentage
     ) ERC4626(_asset) ERC20("Vault Trans Fund Token", "vTFT") {
         i_vaultOwner = payable(msg.sender);
-        s_entryFeeBasicPoints = _basicPoints;
+        s_entryFeeBasicPoints = _feeBasicPoints;
         i_ownerSharesPercentage = _ownerSharesPercentage;
+    }
+
+    modifier isVaultFull(uint256 assetsOrShares) {
+        if (_msgSender() != i_vaultOwner) {
+            if (assetsOrShares > s_totalSharesCanMint) {
+                revert FundVault__VaultIsFull();
+            }
+        }
+        _;
+    }
+
+    modifier isOwnerCanWithdraw(uint256 assetsOrShares) {
+        if (
+            _msgSender() == i_vaultOwner &&
+            assetsOrShares < _amountOwnerCanWithDraw()
+        ) {
+            revert FundVault__OwnerCantWithdraw();
+        }
+        _;
     }
 
     /**
@@ -32,21 +55,16 @@ contract FundVault is ERC4626Fees {
     function deposit(
         uint256 assets,
         address receiver
-    ) public virtual override returns (uint256) {
+    ) public virtual override isVaultFull(assets) returns (uint256) {
         require(
             assets <= maxDeposit(receiver),
             "ERC4626: deposit more than max"
         );
 
-        uint256 shares;
-        if (_msgSender() == i_vaultOwner) {
-            shares = assets;
-        } else {
-            shares = previewDeposit(assets);
-        }
+        uint256 shares = previewDeposit(assets);
 
         _deposit(_msgSender(), receiver, assets, shares);
-        _afterDeposit(assets, shares);
+        _afterDeposit(shares);
 
         return shares;
     }
@@ -55,12 +73,12 @@ contract FundVault is ERC4626Fees {
     function mint(
         uint256 shares,
         address receiver
-    ) public virtual override returns (uint256) {
+    ) public virtual override isVaultFull(shares) returns (uint256) {
         require(shares <= maxMint(receiver), "ERC4626: mint more than max");
 
         uint256 assets = previewMint(shares);
         _deposit(_msgSender(), receiver, assets, shares);
-        _afterDeposit(assets, shares);
+        _afterDeposit(shares);
 
         return assets;
     }
@@ -70,7 +88,7 @@ contract FundVault is ERC4626Fees {
         uint256 shares,
         address receiver,
         address owner
-    ) public virtual override returns (uint256) {
+    ) public virtual override isOwnerCanWithdraw(shares) returns (uint256) {
         require(shares <= maxRedeem(owner), "ERC4626: redeem more than max");
 
         uint256 assets = previewRedeem(shares);
@@ -85,7 +103,7 @@ contract FundVault is ERC4626Fees {
         uint256 assets,
         address receiver,
         address owner
-    ) public virtual override returns (uint256) {
+    ) public virtual override isOwnerCanWithdraw(assets) returns (uint256) {
         require(
             assets <= maxWithdraw(owner),
             "ERC4626: withdraw more than max"
@@ -114,16 +132,17 @@ contract FundVault is ERC4626Fees {
     //    uint256 sharesBalance = IERC20(address(this)).balanceOf(i_vaultOwner);
     // }
 
-    function _amountSharesCanMint() internal view returns (uint256) {
-        uint256 investorSharesPercentage = 100 - i_ownerSharesPercentage;
-        uint256 amountSharesCanMint = (s_ownerShares *
-            investorSharesPercentage) / 100;
-        return amountSharesCanMint;
-    }
-
-    function _afterDeposit(uint256 assets, uint256 shares) internal virtual {
-        if (msg.sender == i_vaultOwner) {
+    function _afterDeposit(uint256 shares) internal virtual {
+        if (_msgSender() == i_vaultOwner && i_ownerSharesPercentage > 0) {
             s_ownerShares += shares;
+            s_totalSharesCanMint =
+                (s_totalSharesCanMint +
+                    ((shares / i_ownerSharesPercentage) * 100)) -
+                shares;
+        } else {
+            s_totalSharesCanMint =
+                s_totalSharesCanMint -
+                getTotalSharesMinted();
         }
     }
 
@@ -131,11 +150,64 @@ contract FundVault is ERC4626Fees {
         // trước khi rút tiền, tính toán một phần tiền lãi cho owner của fund, chuyển phần tiền lãi đó dưới dạng vToken để gửi cho owner
     }
 
+    function _afterWithdraw(uint256 shares) internal virtual {
+        if (_msgSender() == i_vaultOwner && i_ownerSharesPercentage > 0) {
+            require(shares <= s_ownerShares, "Insufficient owner shares");
+            s_ownerShares -= shares;
+            s_totalSharesCanMint =
+                s_totalSharesCanMint +
+                ((shares / i_ownerSharesPercentage) * 100);
+        } else {
+            s_totalSharesCanMint =
+                s_totalSharesCanMint -
+                getTotalSharesMinted();
+        }
+    }
+
+    function _amountOwnerCanWithDraw() internal view returns (uint256) {
+        if (i_ownerSharesPercentage == 0) {
+            return s_ownerShares;
+        }
+
+        if (
+            _actualOwnerSharesPercentage() > i_ownerSharesPercentage &&
+            _actualOwnerSharesPercentage() < 10000
+        ) {
+            // uint256 percentageCanWithdraw = _actualOwnerSharesPercentage() -
+            //     i_ownerSharesPercentage;
+
+            uint256 amountCanWithdraw = s_ownerShares -
+                ((_totalAmountUserMinted() * i_ownerSharesPercentage) / 100);
+
+            return amountCanWithdraw;
+        }
+
+        return s_ownerShares;
+    }
+
+    function _actualOwnerSharesPercentage()
+        internal
+        view
+        returns (uint256 percentageInBasisPoints)
+    {
+        percentageInBasisPoints =
+            (s_ownerShares * 10000) /
+            getTotalSharesMinted();
+    }
+
+    function _totalAmountUserMinted() internal view returns (uint256) {
+        return getTotalSharesMinted() - s_ownerShares;
+    }
+
     function getTotalSharesMinted() public view returns (uint256) {
         return IERC4626(address(this)).totalSupply();
     }
 
-    function getAmountSharesCanMint() public view returns (uint256) {
-        return _amountSharesCanMint();
+    function getActualOwnerPercentage() public view returns (uint256) {
+        return _actualOwnerSharesPercentage();
+    }
+
+    function getAmountOwnerCanWithdraw() public view returns (uint256) {
+        return _amountOwnerCanWithDraw();
     }
 }
--- smart-contract/test/Units/FundVaultTest.t.sol
+++ smart-contract/test/Units/FundVaultTest.t.sol
@@ -47,7 +47,7 @@ contract FundVaultTest is Test {
         _;
     }
 
-    modifier investorDeepositedToVault() {
+    modifier investorDepositToVault() {
         vm.startPrank(investor);
         usdc.approve(address(fundVault), FAUCET_AMOUNT);
         fundVault.deposit(FAUCET_AMOUNT, investor);
@@ -59,34 +59,171 @@ contract FundVaultTest is Test {
         public
         fundManagerDepositToVault
     {
-        assert(FUND_MANAGER_AMOUNT == fundVault.s_ownerShares());
+        uint256 fee = _feeOnTotal(FUND_MANAGER_AMOUNT);
+        assert(FUND_MANAGER_AMOUNT - fee == fundVault.s_ownerShares());
     }
 
-    function testCanDepositToFundVault() public investorDeepositedToVault {
+    function testCanDepositToFundVault()
+        public
+        fundManagerDepositToVault
+        investorDepositToVault
+    {
         uint256 investorBalance = IERC4626(fundVault).balanceOf(investor);
         console.log("Investor Balance: ", investorBalance);
-        uint256 feeOnTotal = FAUCET_AMOUNT.mulDiv(
-            FEE_BASIS_POINT,
-            FEE_BASIS_POINT + _BASIS_POINT_SCALE,
-            Math.Rounding.Ceil
-        );
+        uint256 feeOnTotal = _feeOnTotal(FAUCET_AMOUNT);
         console.log("Fee on Total: ", feeOnTotal);
-        uint256 vaultTotalSupply = IERC4626(fundVault).totalSupply();
 
         assert(FAUCET_AMOUNT - feeOnTotal == investorBalance);
-        assert(vaultTotalSupply == investorBalance);
+    }
+
+    function testCantDepositIfVaultFull() public {
+        vm.startPrank(fundManager);
+        usdc.mint(5e18);
+        usdc.approve(address(fundVault), 5e18);
+        fundVault.deposit(5e18, fundManager);
+        vm.stopPrank();
+
+        uint256 ownerShares = fundVault.s_ownerShares();
+        console.log("Owner Shares", ownerShares);
+
+        uint256 totalSharesCanMint = fundVault.s_totalSharesCanMint();
+        console.log("Total Shares Can Mint", totalSharesCanMint);
+
+        vm.startPrank(investor);
+        usdc.faucet();
+        usdc.approve(address(fundVault), FAUCET_AMOUNT);
+        vm.expectRevert(FundVault.FundVault__VaultIsFull.selector);
+        fundVault.deposit(FAUCET_AMOUNT, investor);
+        vm.stopPrank();
     }
 
     function testGetTotalSharesCanMint() public fundManagerDepositToVault {
-        uint256 investorSharesPercentage = 100 - FUND_MANAGER_SHARES_PERCENTAGE;
+        uint256 totalSharesCanMintFirst = fundVault.s_totalSharesCanMint();
+        console.log("Total Shares First: ", totalSharesCanMintFirst);
         uint256 ownerShares = fundVault.s_ownerShares();
-        console.log("Total Owner Shares", ownerShares);
-        uint256 totalSharesCanMint = (ownerShares * investorSharesPercentage) /
-            100;
+        console.log("Owner Shares: ", ownerShares);
 
-        uint256 totalSharesCanMintExpected = fundVault.getAmountSharesCanMint();
-        console.log("Total Shares Can Deposit", totalSharesCanMintExpected);
+        vm.startPrank(investor);
+        usdc.faucet();
+        usdc.faucet();
+        usdc.approve(address(fundVault), FAUCET_AMOUNT + FAUCET_AMOUNT);
+        fundVault.deposit(FAUCET_AMOUNT + FAUCET_AMOUNT, investor);
+        vm.stopPrank();
 
-        assert(totalSharesCanMint == totalSharesCanMintExpected);
+        vm.startPrank(fundManager);
+        usdc.mint(FUND_MANAGER_AMOUNT - 5000e18);
+        usdc.approve(address(fundVault), FUND_MANAGER_AMOUNT - 5000e18);
+        fundVault.deposit(FUND_MANAGER_AMOUNT - 5000e18, fundManager);
+        vm.stopPrank();
+
+        uint256 totalSharesCanMintSecond = fundVault.s_totalSharesCanMint();
+        console.log("Total Shares Second: ", totalSharesCanMintSecond);
+        uint256 ownerSharesSecond = fundVault.s_ownerShares();
+        console.log("Owner Shares: ", ownerSharesSecond);
+
+        vm.startPrank(investor);
+        usdc.faucet();
+        usdc.faucet();
+        usdc.approve(address(fundVault), FAUCET_AMOUNT + FAUCET_AMOUNT);
+        fundVault.deposit(FAUCET_AMOUNT + FAUCET_AMOUNT, investor);
+        vm.stopPrank();
+
+        uint256 totalSharesCanMintThird = fundVault.s_totalSharesCanMint();
+        console.log("Total Shares Third", totalSharesCanMintThird);
+        uint256 ownerSharesThird = fundVault.s_ownerShares();
+        console.log("Owner Shares: ", ownerSharesThird);
+    }
+
+    function testGetOwnerActualPercentage()
+        public
+        fundManagerDepositToVault
+        investorDepositToVault
+    {
+        uint256 totalSharesSupply = fundVault.getTotalSharesMinted();
+        console.log("Total Shares Minted: ", totalSharesSupply);
+
+        uint256 fundManagerShares = FUND_MANAGER_AMOUNT -
+            _feeOnTotal(FUND_MANAGER_AMOUNT);
+        console.log("Fund Manager Shares", fundManagerShares);
+
+        uint256 investorShares = IERC4626(address(fundVault)).balanceOf(
+            investor
+        );
+
+        console.log("Investor Shares", investorShares);
+
+        assert(
+            IERC4626(address(fundVault)).totalSupply() ==
+                fundManagerShares + investorShares
+        );
+
+        uint256 expectedPercentage = (fundManagerShares * 10000) /
+            IERC4626(address(fundVault)).totalSupply();
+
+        console.log("Expected Fund Manager Percentage: ", expectedPercentage);
+
+        uint256 actualPercentage = fundVault.getActualOwnerPercentage();
+
+        console.log(
+            "Actual Fund Manager Shares Percentage: ",
+            actualPercentage
+        );
+
+        assert(expectedPercentage == actualPercentage);
+    }
+
+    function testGetAmountOwnerCanWithdraw()
+        public
+        fundManagerDepositToVault
+        investorDepositToVault
+    {
+        uint256 totalSharesSupply = fundVault.getTotalSharesMinted();
+
+        console.log("Total Shares Minted: ", totalSharesSupply);
+
+        uint256 amountOwnerShares = IERC4626(address(fundVault)).balanceOf(
+            fundManager
+        );
+
+        console.log("Fund Manager Shares", amountOwnerShares);
+
+        uint256 amountOwnerCanWithdraw = fundVault.getAmountOwnerCanWithdraw();
+
+        console.log("Amount Fund Manager Can Withdraw", amountOwnerCanWithdraw);
+
+        uint256 amountSharesOfOwnerAfterWithdraw = amountOwnerShares -
+            amountOwnerCanWithdraw;
+
+        console.log(
+            "Amount Shares Of Owner After Withdraw",
+            amountSharesOfOwnerAfterWithdraw
+        );
+
+        uint256 amountInvestorShares = IERC4626(address(fundVault)).balanceOf(
+            investor
+        );
+
+        console.log("Amount Investor Shares: ", amountInvestorShares);
+
+        uint256 amountFundManagerMustCommited = (amountInvestorShares *
+            FUND_MANAGER_SHARES_PERCENTAGE) / 100;
+
+        console.log(
+            "Amount Shares Fund Manager Must Commited",
+            amountFundManagerMustCommited
+        );
+
+        assert(
+            amountSharesOfOwnerAfterWithdraw == amountFundManagerMustCommited
+        );
+    }
+
+    function _feeOnTotal(uint256 amount) internal pure returns (uint256) {
+        return
+            amount.mulDiv(
+                FEE_BASIS_POINT,
+                FEE_BASIS_POINT + _BASIS_POINT_SCALE,
+                Math.Rounding.Ceil
+            );
     }
 }
